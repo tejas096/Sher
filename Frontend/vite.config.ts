c128ce56b74662e853938af384a9c3c2876b3534
Tejas Jain <tejasjain096@gmail.com>
2025-08-21 17:25:37 +0530

    Initial commit: frontend + backend + vercel setup

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -4,6 +4,5 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  server: { host: true, port: 5173 },
   plugins: [react(), tailwindcss()],
 });
