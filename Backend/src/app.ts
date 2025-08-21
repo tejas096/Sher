1957625b0c74e576d71b82b93eab3910c2dc1d15
Tejas Jain <tejasjain096@gmail.com>
2025-08-21 22:47:35 +0530

    Initial commit: frontend + backend + vercel setup

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -23,7 +23,7 @@ app.use(passport.initialize());
 app.use(helmet());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: `${process.env.CLIENT_URL}`,
     credentials: true,
   })
 );
